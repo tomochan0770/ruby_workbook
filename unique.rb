abf33b1de00b1ae0a2b1efccb6c072a2d282d6ff
tomochan0770 <tomochan0770@gmail.com>
2021-02-14 13:20:56 +0900

    unique commit

---
+++ unique.rb
@@ -0,0 +1,13 @@
+line = gets
+
+res = []
+isSub = false
+for i in 0..line.length - 1
+	if (line[i] != nil && line[i] != "\n")
+		for j in 0..res.length - 1
+			if (res[j] == line[i])
+				
+	end
+end
+
+p res
